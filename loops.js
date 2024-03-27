05a7814e1117e3fd460dbdb60f6eb6b553038222
HA-Sajib <mesajib2020@gmail.com>
2024-03-27 15:39:01 +0600

    updater

--- loops.js
+++ loops.js
@@ -1,4 +1,14 @@
-console.log('10 + 10 = ' + (10 + 10));
-console.log("20 - 10 = " + (20-10))
-console.log("20 * 10 = " + (20*10))
-console.log("20 / 10 = " + (20/10))
+
+// console.log('10 + 10 = ' + (10 + 10));
+// console.log("20 - 10 = " + (20-10))
+// console.log("20 * 10 = " + (20*10))
+// console.log("20 / 10 = " + (20/10))
+
+let a = 40;
+let b = 20;
+let sum = a+b;
+
+console.log(a + " + " + b + " = " + sum)
+console.log(a + " - " + b +" = " + (a-b))
+console.log(a + " * " + b + " = " + (a*b))
+console.log(a + " / " + b + " = " + (a/b))
