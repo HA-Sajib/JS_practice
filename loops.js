833765f865e99959459101dbc488cdbe6ca630c4
HA-Sajib <mesajib2020@gmail.com>
2024-03-27 15:54:35 +0600

    Data Type

--- loops.js
+++ loops.js
@@ -4,11 +4,24 @@
 // console.log("20 * 10 = " + (20*10))
 // console.log("20 / 10 = " + (20/10))
 
-let a = 40;
-let b = 20;
-let sum = a+b;
+// let a = 40;
+// let b = 20;
+// let sum = a+b;
 
-console.log(a + " + " + b + " = " + sum)
-console.log(a + " - " + b +" = " + (a-b))
-console.log(a + " * " + b + " = " + (a*b))
-console.log(a + " / " + b + " = " + (a/b))
+// console.log(a + " + " + b + " = " + sum)
+// console.log(a + " - " + b +" = " + (a-b))
+// console.log(a + " * " + b + " = " + (a*b))
+// console.log(a + " / " + b + " = " + (a/b))
+
+let a = 10
+let b = 20.3
+let c = "hello"
+let d = true
+let f = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
