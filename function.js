901c502e2f8a10391cb149c77b52f98784105b2a
HA-Sajib <mesajib2020@gmail.com>
2024-03-31 16:00:53 +0600

    rectangle paraeter

--- function.js
+++ function.js
@@ -21,15 +21,23 @@
 // let r2 = paraMeterOfRactangle(2,2);
 // console.log(r2)
 
-function RectangleParameter(h,w){
-    let height = 2*h;
-    let weidth = 2*h;
-    let paraMeter = height + weidth;
+// function RectangleParameter(h,w){
+//     let height = 2*h;
+//     let weidth = 2*h;
+//     let paraMeter = height + weidth;
+//     return paraMeter;
+// }
+
+// let r1 = RectangleParameter(10,20)
+// let r2 = RectangleParameter(30,20)
+// let r3 = RectangleParameter(40,22)
+
+// console.log(r1,r2,r3)
+
+function RectangleParameter(height,width){
+    let paraMeter = 2*(height + width)// (2*height) + (2*width)
     return paraMeter;
 }
 
-let r1 = RectangleParameter(10,20)
-let r2 = RectangleParameter(30,20)
-let r3 = RectangleParameter(40,22)
-
-console.log(r1,r2,r3)
+let paraMeter = RectangleParameter(10,20)
+console.log(paraMeter)
