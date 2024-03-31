6bd1a1aba9743aafadd6fa070d85d0f5397982e8
HA-Sajib <mesajib2020@gmail.com>
2024-03-31 15:14:11 +0600

    function

--- function.js
+++ function.js
@@ -1,10 +1,10 @@
-function sum(a,b){
-    const c = a+b;
-    return c;
-}
+// function sum(a,b){
+//     const c = a+b;
+//     return c;
+// }
 
-let r1 = sum(10,20)
-let r2 = sum(30,40)
-let r3 = sum(50,60)
+// let r1 = sum(10,20)
+// let r2 = sum(30,40)
+// let r3 = sum(50,60)
 
-console.log(r1,r2,r3)
+// console.log(r1,r2,r3)
